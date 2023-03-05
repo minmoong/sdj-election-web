9847b4613ab7b74901293007c58a6b3b30741a10
Minkyu Kim <kim774634@gmail.com>
2023-03-05 09:54:21 +0000

    완성

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,20 @@
 <div class="root">
   <slot />
+  <div class="madeby">Made by 10603 김민규</div>
 </div>
 
 <style>
   .root {
     width: 100%;
     height: 100%;
+  }
+
+  .madeby {
+    position: absolute;
+    bottom: 20px;
+    right: 20px;
+    color: #275efe;
+    font-size: 20px;
+    opacity: 0.7;
   }
 </style>
